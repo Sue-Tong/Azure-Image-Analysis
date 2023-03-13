b89c4b1e9222c02e5b85309a57a0e416c8ff0d44
Tong Su <90353234+Sue-Tong@users.noreply.github.com>
2023-03-13 02:10:39 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,18 +7,18 @@
 #SBATCH --mail-type=ALL
 
 module load python/3.8
-virtualenv --no-download env
+# virtualenv --no-download env
 source env/bin/activate
 module load python/3.8
 module load cuda/11.4 gcc/9.3.0 arrow/8.0.0
 
-pip install .
-pip install azure-cognitiveservices-vision-computervision
-pip install msrest
-pip install Pillow
-pip install pandas
-pip install numpy
-pip install azure-storage-blob
-pip install azure-cognitiveservices-vision-face
+# pip install .
+# pip install azure-cognitiveservices-vision-computervision
+# pip install msrest
+# pip install Pillow
+# pip install pandas
+# pip install numpy
+# pip install azure-storage-blob
+# pip install azure-cognitiveservices-vision-face
 
 python3 image.py
